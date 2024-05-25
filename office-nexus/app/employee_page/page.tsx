cb967e012b32e4174db14470e75c0331935ccf86
Kean1202 <18221148@std.stei.itb.ac.id>
2024-05-25 15:10:11 +0700

    added the pages for officenexus

---
+++ office-nexus/app/employee_page/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div className="flex w-80 h-20">
+        <h1 className="headline_text text-slate-500">Bagian Haidar</h1>
+      </div>
+  );
+}
